2310ac09e54cceb4e1d1b8c56067103b4ee8b223
Saim Amir <saimxamir@gmail.com>
2023-07-29 15:41:10 -0400

    complete ex. 1 creating stores

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -1,6 +1,13 @@
 require_relative '../setup'
 
-puts "Exercise 1"
-puts "----------"
+puts 'Exercise 1'
+puts '----------'
 
 # Your code goes below here ...
+# Create 3 stores using the create class method of the Store model
+Store.create(name: 'Burnaby', annual_revenue: 300_000, mens_apparel: true, womens_apparel: true)
+Store.create(name: 'Richmond', annual_revenue: 1_260_000, womens_apparel: true)
+Store.create(name: 'Gastown', annual_revenue: 190_000, mens_apparel: true)
+
+# Output the number of stores using the count method
+puts Store.count
