bb58dfcfe50593e5fefc1be5a43a53a01734cbd9
Saim Amir <saimxamir@gmail.com>
2023-07-29 15:49:12 -0400

    complete ex.2 update store name

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -1,7 +1,13 @@
 require_relative '../setup'
 require_relative './exercise_1'
 
-puts "Exercise 2"
-puts "----------"
+puts 'Exercise 2'
+puts '----------'
 
 # Your code goes here ...
+
+@store1 = Store.find(1)
+@store2 = Store.find(2)
+
+@store1.name = 'Dior'
+@store1.save
