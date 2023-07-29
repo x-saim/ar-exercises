d5dc579d9c0a6f5cdba54cd3e1c7089c061390fe
Saim Amir <saimxamir@gmail.com>
2023-07-29 16:53:14 -0400

    complete ex.6: one to many association

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -5,7 +5,12 @@ require_relative './exercise_3'
 require_relative './exercise_4'
 require_relative './exercise_5'
 
-puts "Exercise 6"
-puts "----------"
+puts 'Exercise 6'
+puts '----------'
 
 # Your code goes here ...
+
+@store1.employees.create(first_name: 'Khurram', last_name: 'Virani', hourly_rate: 60)
+@store2.employees.create(first_name: 'Alice', last_name: 'Smith', hourly_rate: 55)
+@store1.employees.create(first_name: 'Emily', last_name: 'Johnson', hourly_rate: 55)
+@store2.employees.create(first_name: 'Daniel', last_name: 'Smith', hourly_rate: 63)
