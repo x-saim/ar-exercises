c381dc96f8df4a0f3d30a4503ea415ec50b4c6d3
Saim Amir <saimxamir@gmail.com>
2023-07-29 16:38:18 -0400

    complete ex. 5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -4,7 +4,15 @@ require_relative './exercise_2'
 require_relative './exercise_3'
 require_relative './exercise_4'
 
-puts "Exercise 5"
-puts "----------"
+puts 'Exercise 5'
+puts '----------'
 
 # Your code goes here ...
+
+@total_revenue = Store.sum(:annual_revenue)
+puts "The total revenue for all stores combined is $#{@total_revenue}."
+puts "The average revenue for all stores combined is $#{@total_revenue / Store.count}."
+
+@store_million = Store.where('annual_revenue > ?',
+                             1_000_000).count
+puts "The number of stores that are generating $1M or more in annual sales is #{@store_million}."
