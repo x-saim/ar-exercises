215a08279e72c83402de1df404d48b2d90e732ee
Saim Amir <saimxamir@gmail.com>
2023-07-29 18:27:35 -0400

    complete ex.7

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -6,7 +6,26 @@ require_relative './exercise_4'
 require_relative './exercise_5'
 require_relative './exercise_6'
 
-puts "Exercise 7"
-puts "----------"
+puts 'Exercise 7'
+puts '----------'
 
 # Your code goes here ...
+# Assuming you already have the Store model defined with the validations
+
+# Ask the user for a store name and store it in a variable
+print 'Enter the store name: '
+store_name = gets.chomp
+
+# Attempt to create a store with the inputted name but leave out the other fields
+store = Store.new(name: store_name)
+
+# Try to save/create the record
+if store.save
+  puts 'Store was successfully created.'
+else
+  # Display the error messages
+  puts 'Failed to create the store:'
+  store.errors.full_messages.each do |message|
+    puts message
+  end
+end
