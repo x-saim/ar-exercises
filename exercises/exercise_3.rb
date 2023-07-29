d2918aad8701a15e1208c08903629398a1ff07c5
Saim Amir <saimxamir@gmail.com>
2023-07-29 15:51:27 -0400

    complete ex.3 delete store id =3

--- exercises/exercise_3.rb
+++ exercises/exercise_3.rb
@@ -2,7 +2,12 @@ require_relative '../setup'
 require_relative './exercise_1'
 require_relative './exercise_2'
 
-puts "Exercise 3"
-puts "----------"
+puts 'Exercise 3'
+puts '----------'
 
 # Your code goes here ...
+
+@store3 = Store.find(3)
+@store3.destroy
+
+puts Store.count
